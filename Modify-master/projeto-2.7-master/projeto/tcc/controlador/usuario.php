fa3f964f2b28be391c4a533098784610a4f23965
JuliaDSolano <juliaduartesolano@gmail.com>
2018-08-10 11:04:35 -0300

    Add files via upload

---
+++ Modify-master/projeto-2.7-master/projeto/tcc/controlador/usuario.php
@@ -0,0 +1,28 @@
+
+<?php
+require_once '../modelo/CRUDlogin.php';
+
+if (isset($_GET['acao'])){
+    $acao = $_GET['acao'];
+}else{
+    $acao = 'verifica';
+}
+
+switch ($acao) {
+    case 'verifica':
+        $crud = new CrudLogin();
+
+        if (isset($_POST['gravar'])) {
+            $usuarios = $crud->GetUsuarios();
+
+            foreach ($usuarios as $usuario) {
+                
+                if ($_POST['email'] == $usuario->email and $_POST['senha'] == $usuario->senha) {
+                    echo 'oi';
+                    header('../views/index.html');
+                }
+            }
+        } else {
+            include '../login.phtml';
+        }
+}
